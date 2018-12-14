f144bd1cf2a5d228e01cbc1613c753b7073dcc5a
Saul Amster <saulamster@gmail.com>
2018-12-14 15:16:53 -0500

    first pass of player UI

--- project/MatchMakerRevised.cpp
+++ project/MatchMakerRevised.cpp
@@ -53,6 +53,7 @@ void MatchMakerRevised::playMatchesQueue(int numOfMatches, std::string queueName
     }
 
 
+
     int total=playQueue->getCount();
     bool even;
 
@@ -65,8 +66,8 @@ void MatchMakerRevised::playMatchesQueue(int numOfMatches, std::string queueName
         total=total/2;
     }
     if( numOfMatches>total){
-        std::cout << "Entered Match Amount is greater than that of players in " +  queueName + " queue"<< std::endl;
-        std::cout << "Played " + std::to_string(total) + " instead" << std::endl;
+        //std::cout << "Entered Match Amount is greater than that of players in " +  queueName + " queue"<< std::endl;
+        //std::cout << "Played " + std::to_string(total) + " instead" << std::endl;
         for (int f = 0; f < total; f++) {
             Game *testGame = new Game(playQueue->dequeue()->getPlayer(), playQueue->dequeue()->getPlayer());
             testGame->collectGuesses();
